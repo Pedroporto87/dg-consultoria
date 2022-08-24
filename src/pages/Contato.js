4b20260b31ca3663490719dd0346343ee5119513
Pedro Porto Pereira de Souza <pedroporto_8@hotmail.com>
2022-08-24 12:41:35 -0300

    finalizada a animação do navbar

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Contato() {
+ const [form, setForm] = useState({
+  nome:'',
+  email:'',
+  assunto: '',
+  telefone:'',
+ })
   return (
     <h1>Contato</h1>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,25 @@ import '../styles/Home.css'
 
 export default function Home() {
   return (
-    
-    <section className='conteiner'>
-      <h1 className='titulo'>Sobre nós</h1>
-      <p className='sobre-nos-1'>
-        A DG Ambiental é uma empresa do ramo de Consultoria e Assessoria 
-        Técnica Ambiental, fundada em 2022, especializada em prestação de
-        serviços ligados a todas as etapas do <b>Licenciamento Ambiental</b> de
-        empreedimentos de pequeno, médio ou grande porte das mais diversas
-        áreas de negócio, tendo como diferencial a expertise de seus sócios
-        no ramo de atuação.
-      </p>
-      <p className='sobre-nos-2'>
-        Nosso foco é garantir a obtenção e a manutenção de licenças ambientais
-        e atender condicionantes e exigências ambientais de que sua empresa
-        e seu negócio precisem. Atuamos na elaboração e execução de diversos
-        tipos de projetos e estudos ambientais, contribuindo para a conformidade
-        ambiental de nossos clientes.
-      </p>
-    </section>
+    <div className='home-conteiner'>
+      <section className='conteiner'>
+        <h1 className='titulo'>Sobre nós</h1>
+        <p className='sobre-nos-1'>
+              A DG Ambiental é uma empresa do ramo de Consultoria e Assessoria 
+          Técnica Ambiental, fundada em 2022, especializada em prestação de
+          serviços ligados a todas as etapas do <b>Licenciamento Ambiental</b> de
+          empreedimentos de pequeno, médio ou grande porte das mais diversas
+          áreas de negócio, tendo como diferencial a expertise de seus sócios
+          no ramo de atuação.
+        </p>
+        <p className='sobre-nos-2'>
+              Nosso foco é garantir a obtenção e a manutenção de licenças ambientais
+          e atender condicionantes e exigências ambientais de que sua empresa
+          e seu negócio precisem. Atuamos na elaboração e execução de diversos
+          tipos de projetos e estudos ambientais, contribuindo para a conformidade
+          ambiental de nossos clientes.
+        </p>
+      </section>
+    </div>
     )
 }
